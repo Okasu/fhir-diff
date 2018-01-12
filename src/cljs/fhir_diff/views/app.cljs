92c937b5aba30de2b7e4b4dc090fa1b01c28edd8
George Semenov <me@gsemenov.com>
2018-01-12 15:07:10 +0300

    Remove redundant let binding

--- src/cljs/fhir_diff/views/app.cljs
+++ src/cljs/fhir_diff/views/app.cljs
@@ -13,14 +13,13 @@
    [:a (tab-attributes path) name]])
 
 (defn app []
-  (let [current-view (rf/subscribe [::routes/current-view])]
-    [:div
-     [:nav {:class "navbar navbar-dark bg-dark"}
-      [:a {:class "navbar-brand" :href "#/"} "FHIR Diff"]]
-     [:ul {:class "nav nav-tabs"}
-      [tab-item "/" "Upload"]
-      (when @(rf/subscribe [::diff-subs/diff])
-        [tab-item "/diff" "Diff"])
-      (when-let [resource @(rf/subscribe [::diff-subs/selected-resource])]
-        [tab-item (str "/diff/" resource) resource])]
-     @current-view]))
+  [:div
+   [:nav {:class "navbar navbar-dark bg-dark"}
+    [:a {:class "navbar-brand" :href "#/"} "FHIR Diff"]]
+   [:ul {:class "nav nav-tabs"}
+    [tab-item "/" "Upload"]
+    (when @(rf/subscribe [::diff-subs/diff])
+      [tab-item "/diff" "Diff"])
+    (when-let [resource @(rf/subscribe [::diff-subs/selected-resource])]
+      [tab-item (str "/diff/" resource) resource])]
+   @(rf/subscribe [::routes/current-view])])
